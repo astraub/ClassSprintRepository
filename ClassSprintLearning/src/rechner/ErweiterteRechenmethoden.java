7b8af0a845e2c69ca00a4caf5ad48f60ab09b7bf
sirswagalot <nix@da.de>
2015-05-05 13:46:33 +0200

    asfsdgsdgsdd f

---
+++ ClassSprintLearning/src/rechner/ErweiterteRechenmethoden.java
@@ -0,0 +1,5 @@
+package rechner;
+
+public interface ErweiterteRechenmethoden {
+	public double berechnen(int arg);
+}
---
+++ ClassSprintLearning/src/rechner/Quadratwurzel.java
@@ -0,0 +1,11 @@
+package rechner;
+
+public class Quadratwurzel implements ErweiterteRechenmethoden{
+
+	@Override
+	public double berechnen(int arg) {
+			
+		return Math.sqrt(arg);
+	}
+
+}
