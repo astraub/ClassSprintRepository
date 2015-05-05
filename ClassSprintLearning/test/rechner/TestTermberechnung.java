73d6b0b007e6c8d446cd8f2e184d8f738568d087
AST <adrian.straub@hs-weingarten.de>
2015-05-05 15:20:14 +0200

    testcase for termberechnung.

--- ClassSprintLearning/test/rechner/TestTermberechnung.java
+++ ClassSprintLearning/test/rechner/TestTermberechnung.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class TestTermberechnung {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTermberechnung(){
+		
+		char[] rechenzeichen = new char[]{'+','+','-'};
+		int[] zahlen = new int[]{4,3,2,1};
+		
+		assertEquals(8, Termberechnung.berechnen(zahlen, rechenzeichen));
 	}
 
 }
