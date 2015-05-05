2ff02e28e879f936ed4fbefa41c30f5da7f0aabe
AST <adrian.straub@hs-weingarten.de>
2015-05-05 12:35:16 +0200

    Updatet Testcase.

---
+++ ClassSprintLearning/test/rechner/TestDividieren.java
@@ -0,0 +1,21 @@
+package rechner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDividieren {
+
+	private Dividieren dividieren = new Dividieren();
+	
+	@Test
+	public void testBerechnen() {
+		assertEquals(3, dividieren.berechnen(6, 2));
+		assertEquals(7, dividieren.berechnen(14, 2));
+		assertEquals(11, dividieren.berechnen(66, 6));
+		assertNotEquals(3, dividieren.berechnen(2, 1));
+		assertNotEquals(7, dividieren.berechnen(5, 1));
+		assertNotEquals(11, dividieren.berechnen(67, 6));
+	}
+
+}
